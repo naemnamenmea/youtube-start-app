16d0299efe20466fc08b3824954817b51e103d4d
Andrei Kontrolskii <kontrolskiy@mail.ru>
2019-06-18 03:36:15 +0300

    -

---
+++ server (.Net Core)/CoreWebAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using CoreWebAPI.Entities;
+using CoreWebAPI.Helpers;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CoreWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                })
+                //.UseUrls("http://localhost:4000")
+                .Build();
+    }
+}
